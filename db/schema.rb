38ead32ef3dd6d664825fe495478708bea05c3d5
Joao Mateus <jaomateus@gmail.com>
2022-11-21 18:07:42 +0100

    create floaty model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_162938) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_170356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "floaties", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.string "category"
+    t.integer "price"
+    t.string "city"
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_floaties_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_162938) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "floaties", "users"
 end
