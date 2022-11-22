249ee269912b3bc4d3d9afcecd296a9ee60fefc0
Daria Gortchacow <daria.gortchacow@gmail.com>
2022-11-22 12:33:59 +0100

    seed data base ready

--- app/controllers/floaties_controller.rb
+++ app/controllers/floaties_controller.rb
@@ -1,2 +1,15 @@
 class FloatiesController < ApplicationController
+  # def new
+  #   @floaty = Floaty.new
+  # end
+
+  # def create
+  #   @floaty = Floaty.new(floaty_params)
+  #   @floaty.save!
+  # end
+
+  # private
+
+  # def floaty_params
+  #   params.require()
 end
