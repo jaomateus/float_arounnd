050bd4d4eb3e4e8bc89b72eac3a47efe66be965b
Joao Mateus <jaomateus@gmail.com>
2022-11-22 15:24:43 +0100

    create add floaty

--- app/controllers/floaties_controller.rb
+++ app/controllers/floaties_controller.rb
@@ -2,4 +2,24 @@ class FloatiesController < ApplicationController
   def index
     @floaties = Floaty.all
   end
+
+  def new
+    @floaty = Floaty.new
+  end
+
+  def create
+    @floaty = Floaty.new(floaty_params)
+
+    if @floaty.save
+      redirect_to floaty_path(@floaty.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def floaty_params
+    params.require(:floaty).permit(:title, :category, :details, :price, :city, :address)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: "floaties#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :floaties, only: [:index] do
-    resources :bookings, only: [:index]
+  resources :floaties, only: [:index, :new, :create] do
+    resources :bookings, only: [:index, :new, :create]
   end
 
   # Defines the root path route ("/")
