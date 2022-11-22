2260ec8c4f19bbe47806ae0bf8069759c2e1a8be
Joao Mateus <jaomateus@gmail.com>
2022-11-22 13:02:54 +0100

    define bookings index method

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "floaties#index"
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :floaties, only: [:index]
-  
+  resources :floaties, only: [:index] do
+    resources :bookings, only: [:index]
+  end
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
