17cd4b9303c559e0dafb512d143e2e4789609132
Daria Gortchacow <daria.gortchacow@gmail.com>
2022-11-22 13:06:10 +0100

    show page of floaty

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "floaties#index"
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :floaties, only: [:index]
-  
+  resources :floaties, only: [:index, :show]
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
