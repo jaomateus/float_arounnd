b97c1cfd801a122f496664c04923017e95dd5a94
Daria Gortchacow <daria.gortchacow@gmail.com>
2022-11-25 15:09:00 +0100

    end of friday

--- app/javascript/controllers/togglemap_controller.js
+++ app/javascript/controllers/togglemap_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-map"
 export default class extends Controller {
-  static targets = [ "grid" , "map"]
+  static targets = [ "grid" , "map", "button"]
 
   connect() {
     console.log("test")
@@ -11,8 +11,10 @@ export default class extends Controller {
   toggleTargets() {
     if (this.gridTarget.classList.contains("hidden")) {
       this.gridTarget.classList.remove("hidden");
+      this.buttonTarget.innerText = "Show map"
     } else {
       this.gridTarget.classList.add("hidden");
+      this.buttonTarget.innerText = "Show list"
     }
 
     if (this.mapTarget.classList.contains("invisible")) {
