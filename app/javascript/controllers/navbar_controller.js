3118151eafeba523add07f83acbd814c71e37022
Joao Mateus <jaomateus@gmail.com>
2023-01-12 19:04:01 +0100

    finish navbar

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  connect() {
+    console.log('Im working!!!');
+  }
+
+  updateNavbar() {
+    if (window.scrollY >= window.innerHeight) {
+      this.element.classList.add("nabbar_bg")
+    } else {
+      this.element.classList.remove("nabbar_bg")
+    }
+  }
+
+}
